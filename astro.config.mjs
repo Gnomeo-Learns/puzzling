11ccc9f93d0c50719e4038d641cf4c6942a8c505
Joe <josephtruebe+git@gmail.com>
2026-03-16 13:07:05 -0400

    initial commit for puzzling strats site

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// Update 'puzzle-strats' to match your GitHub repo name
+export default defineConfig({
+  site: 'https://gnomeo-learns.github.io',
+  base: '/puzzling',
+});
